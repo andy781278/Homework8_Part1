bc01d2e97b1a16a9c14afce3fb42566debe303a5
Andy <zhumeilun0103@gmail.com>
2024-10-29 00:19:38 -0700

    commit for 1st phase

--- Program.cs
+++ Program.cs
@@ -17,33 +17,53 @@ foreach (Duelist duelist in duelists) {
 
 static void match(Duelist[] duelists) {
 
-    while (true)
+    while (aliveAmt(duelists)>1)
     {
+        //Console.WriteLine("Round Start!");
         foreach (Duelist duelist in duelists)
         {
+            if (!duelist.isAlive()) { continue; }
+            //Console.WriteLine(duelist.getName()+"'s turn!");
             //get greatest
-            Duelist greatest = getGreatest(duelists);
-            //check if won
-            if (greatest.getName() == "unknown" && duelist.isAlive())
-            {
-                duelist.win();
-                return;
-            }
-            else
+            Duelist greatest = getGreatest(duelists,duelist);
+
+            //Console.WriteLine("current strongest is "+greatest.getName());
+
+            if (duelist.isAlive())
             {
                 //shoot
                 duelist.shootAtTarget(greatest);
             }
         }
     }
-
+    getWinner(duelists);
+    foreach (Duelist duelist in duelists) { duelist.revive(); }
 }
 
-static Duelist getGreatest(Duelist[] duelists) {
+static int aliveAmt(Duelist[] duelists) {
+    int amt = 0;
+    foreach (Duelist duelist in duelists) {
+        if (duelist.isAlive()) { amt++; }
+    }
+    return amt;
+}
+
+static void getWinner(Duelist[] duelists) {
+    foreach (Duelist duelist in duelists)
+    {
+        if (duelist.isAlive()) {
+            //Console.WriteLine(duelist.getName()+" wins!\n");
+            duelist.win();
+            return;
+        }
+    }
+}
+
+static Duelist getGreatest(Duelist[] duelists, Duelist turner) {
     Duelist greatest = new Duelist();
     foreach (Duelist duelist in duelists)
     {
-        if (duelist.getChance() > greatest.getChance() && duelist.isAlive() && duelist.getName()!=greatest.getName())
+        if (duelist.getChance() > greatest.getChance() && duelist.isAlive() && duelist.getName()!=turner.getName())
         {
             greatest = duelist;
         }
@@ -65,20 +85,24 @@ class Duelist {
     }
 
     public void shootAtTarget(Duelist target) {
+        //Console.WriteLine(getName() + " shoots at "+target.getName());
         Random rand = new Random();
         double roll = rand.NextDouble();
         if (roll < chance) {
+            //Console.WriteLine(getName() + " hits! " + target.getName()+" dies.");
             target.die();
         }
     }
 
     public string getStats() {
-        return name + " has won " + wonAmt + " games, totaling " + (wonAmt / 10000 * 100) + "% win rate.";
+        return name + " has won " + wonAmt + " games, totaling " + (wonAmt / 10000.0 * 100.0) + "% win rate.";
     }
 
     public void win() { wonAmt++; }
 
     private void die() { alive = false; }
+
+    public void revive() { alive = true; }
 
     public bool isAlive() { return alive; }
 
